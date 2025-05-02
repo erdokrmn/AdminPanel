e3ca765c2d21a5755bc44ad40fd695349a40e29e
Erdinç Karaman <erdokrmn@hotmail.com>
2025-05-02 09:30:51 +0300

    erdokrmn

--- obj/Debug/net8.0/AdminPanel.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdminPanel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminPanel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b0a5b6d417ded15f1de52669e48ec5dac84b69d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminPanel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminPanel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
